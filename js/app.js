22134d6467bb0f322da9d0c5086081f9b7758f41
rakibul181 <rakibulhasan0641@gmail.com>
2022-09-14 14:38:03 +0600

    user image and user undefine ids fixed

--- js/app.js
+++ js/app.js
@@ -51,6 +51,8 @@ const switchTab = (id) => {
 };
 
 const createPost = (post) => {
+    // console.log(post.userImage)
+    const userImg = post.userImage
     const image = post.image;
     const div = document.createElement( "article" );
     div.classList.add( "post" );
@@ -62,7 +64,7 @@ const createPost = (post) => {
                     target="_blank"
                     class="post__avatar"
                   >
-                    <img src="${image}" alt="User Picture" />
+                    <img src="${userImg}" alt="User Picture" />
                   </a>
                   <a href="#" class="post__user">phero</a>
                 </div>
@@ -120,9 +122,9 @@ const createPost = (post) => {
                   <div class="post__description">
                     <small>
                       <a class="post__name--underline" href="#">
-                          ${post.comments?.user}
+                          ${post.comments[0]?.user}
                       </a>
-                      ${post.comments?.text}
+                      ${post.comments[0]?.text}
                     </small>
                   </div>
                   <span class="post__date-time">30 minutes ago</span>
